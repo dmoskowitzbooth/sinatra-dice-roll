516e977e3dc44936872baee0f47b5a243168cc7b
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-11 03:10:20 +0000

    1

--- dice.rb
+++ dice.rb
@@ -1,10 +1,63 @@
 # /dice.rb
 require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get '/' do
-  "Hello World"
-end
+  "<h1>Welcome to the random dice roller</h1>
+    <h3>Select an option from below:</h3>"
+    <div>
+    <ul>
+      <li><a href="/dice/2/6">2 6-sided die</a></li>
+    </ul>
+    end
+    </div>
 
 get '/zebra' do
   "Need a route for each path we want to support"
+end
+
+get '/pup' do
+  "Better not have had to restart for this to pop up"
+end
+
+get("/dice/2/6") do
+  first_die=rand(1..6)
+  second_die=rand(1..6)
+  sum=first_die+second_die
+  outcome="You rolled a #{first_die} on the first die and a #{second_die} on the second die for a total of #{sum}"
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die=rand(1..10)
+  second_die=rand(1..10)
+  sum=first_die+second_die
+  outcome="You rolled a #{first_die} on the first die and a #{second_die} on the second die for a total of #{sum}"
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die=rand(1..20)
+  outcome="You rolled a #{first_die}."
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die=rand(1..4)
+  second_die=rand(1..4)
+  third_die=rand(1..4)
+  fourth_die=rand(1..4)
+  fifth_die=rand(1..4)
+  sum=first_die+second_die+third_die+fourth_die+fifth_die
+  outcome="You rolled a #{first_die} on the first die, a #{second_die} on the second die, a #{third_die} on the third die, a #{fourth_die} on the fourth die, and a #{fifth_die} on the fifth die for a total of #{sum}"
+  "<h1>5d4</h1>
+  <p>#{outcome}</p>"
 end
