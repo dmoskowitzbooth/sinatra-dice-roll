c5df2fb12df7de2fd44d177dc334904d97821476
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-11 14:38:21 +0000

    erb1

--- dice.rb
+++ dice.rb
@@ -23,9 +23,9 @@ get("/dice/2/6") do
   first_die=rand(1..6)
   second_die=rand(1..6)
   sum=first_die+second_die
-  outcome="You rolled a #{first_die} on the first die and a #{second_die} on the second die for a total of #{sum}"
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  @outcome="You rolled a #{first_die} on the first die and a #{second_die} on the second die for a total of #{sum}"
+
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
